91bbd6a027fff546a25a32c45a00fbfd34584cc7
“bcwin-i” <iziqbek@gmail.com>
2022-11-14 11:35:47 +0000

    Latets

--- script.js
+++ script.js
@@ -218,8 +218,8 @@ function startTimer() {
             } else {
               if (active) timer.style.opacity = 1;
             }
-            if (globalClaimCheck(timer)) return
-            
+            if (globalClaimCheck(timer)) return;
+
             timer.style.cursor = "pointer";
             timer.style.whiteSpace = "nowrap";
             if (!timer.dataset.reward) {
@@ -268,7 +268,7 @@ function formatTimerToEarn(img) {
 
     timer.style.cursor = "pointer";
     timer.style.whiteSpace = "nowrap";
-    if (globalClaimCheck(timer)) return
+    if (globalClaimCheck(timer)) return;
     if (!timer.dataset.reward) {
       timer.innerHTML = newReward;
       setTimeout(() => {
@@ -303,13 +303,13 @@ function timerHandler() {
   images = currImg;
 }
 
-function globalClaimCheck(timer){
+function globalClaimCheck(timer) {
   if (globalClaim) {
-    timer.style.width = "min-content"
+    timer.style.width = "min-content";
     timer.innerHTML = newReward;
     return true;
   }
-  return false
+  return false;
 }
 
 function addImage(img) {
@@ -392,6 +392,32 @@ document.onreadystatechange = async () => {
   }
 };
 
+window.addEventListener("beforeinstallprompt", (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  var deferredPrompt = e;
+  console.log("Ready to install");
+  // Update UI to notify the user they can add to home screen
+  const addBtn = document.getElementById("install");
+
+  addBtn.addEventListener("click", (e) => {
+    // hide our user interface that shows our A2HS button
+    addBtn.style.display = "none";
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === "accepted") {
+        console.log("User accepted the A2HS prompt");
+      } else {
+        console.log("User dismissed the A2HS prompt");
+      }
+      deferredPrompt = null;
+    });
+  });
+});
+
 async function generateQRCode(data) {
   // console.log("QR generator called: ", data);
   const container = document.querySelector("#QRContainer");
@@ -428,11 +454,17 @@ async function createDownload() {
   downloadlink2.innerText = "Link device";
   downloadlink2.id = "Kaalaa_auto_link";
 
+  const Installlink = document.createElement("button");
+  Installlink.className = "modal-button";
+  Installlink.id = "install";
+  Installlink.innerText = "Add to homescreen";
+
   await generateQRCode(getCookie("Kaalaa"));
   document.body.appendChild(newdiv);
 
   newdiv.appendChild(downloadlink);
   newdiv.appendChild(downloadlink2);
+  newdiv.appendChild(Installlink);
 }
 
 window.onscroll = async function (e) {
