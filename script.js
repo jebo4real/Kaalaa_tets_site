443fe680b63e61dbc0e4371e44439d7d728b4f2b
“bcwin-i” <iziqbek@gmail.com>
2022-11-14 14:20:19 +0000

    Done

--- script.js
+++ script.js
@@ -409,13 +409,14 @@ window.addEventListener("beforeinstallprompt", (e) => {
 
   addBtn.addEventListener("click", (e) => {
     // hide our user interface that shows our A2HS button
-    addBtn.style.display = "none";
+    
     // Show the prompt
     deferredPrompt.prompt();
     // Wait for the user to respond to the prompt
     deferredPrompt.userChoice.then((choiceResult) => {
       if (choiceResult.outcome === "accepted") {
         console.log("User accepted the A2HS prompt");
+        addBtn.style.display = "none";
       } else {
         console.log("User dismissed the A2HS prompt");
       }
