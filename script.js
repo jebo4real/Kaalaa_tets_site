c5c8703e426426ad291e0d95e2d8a768c2d813d2
bcwin-i <iziqbek@gmail.com>
2022-11-21 18:57:58 +0000

    scale

--- script.js
+++ script.js
@@ -698,8 +698,8 @@ function onHover(e, idPlain) {
   if (active) {
     timer_container.style.opacity = 1;
     if (images[imagesExist].timer !== 0) {
-      timer_container.style.scale = 0;
-      timer_container.style.scale = 1;
+      timer_container.style.transform = 'scale(0)';
+      timer_container.style.transform = 'scale(1)';
       timer_container.innerHTML = kaalaaTimerHandler(
         "kaalaa_max_timer zoom-in-out-box"
       );
