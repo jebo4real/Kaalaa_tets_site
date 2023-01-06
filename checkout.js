76d5e10a5f40bd56ea6adc70ecec76e660e4d308
bcwin-i <iziqbek@gmail.com>
2023-01-06 02:14:14 +0000

    checkout baseUrl

--- checkout.js
+++ checkout.js
@@ -1,8 +1,12 @@
 console.log("KĀĀlĀĀ checkout script initiated");
 
 let rewards = 0;
-const url = ["https://kaalaa-app.herokuapp.com/", "http://localhost:5050/"];
-const baseURL = url[0];
+const url = [
+  "https://kaalaa-server.com/",
+  "http://localhost:5050/",
+  "https://3e14-154-160-18-118.eu.ngrok.io/",
+];
+const baseURL = url[1];
 const auth = {
   username: "a2FhbGFhX2FjY2VzcyB1c2VybmFtZQ==",
   password: "a2FhbGFhX2FjY2VzcyBwYXNzd29yZA==",
